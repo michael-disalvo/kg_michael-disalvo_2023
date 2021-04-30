090c999ca6f815d67e781cb44a7a99c2b4ca557b
michael-disalvo <disamt19@wfu.edu>
2021-04-30 11:39:14 -0400

    Added function to process a number and return its string representation. We loop through arguments in main and add to output string. When a not-a-number is put into the processor, it returns ERROR

--- main.go
+++ main.go
@@ -3,10 +3,42 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"unicode"
 )
 
+var number_names = [10]string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+
+//given command line arguments 250 6 we should print out "TwoFiveZero,Six"
+
 func main() {
+	//get command line arguments and store in args
 	args := os.Args[1:]
-	fmt.Println(strings.Join(args, ","))
+
+	var output string = ""
+	for i, number := range args {
+		//add the string representation to output
+		output += process_number(number)
+		//if we are not at the last arg, add a comma afterwards
+		if i != len(args)-1 {
+			output += ","
+		}
+	}
+
+	fmt.Println(output)
+}
+
+//takes in a number in form of string and returns the string representation of that number ("5" -> "Five")
+func process_number(digit_form string) string {
+	var word_form string = ""
+	//iterate over the characters of the input string
+	for _, c := range digit_form {
+		if unicode.IsDigit(c) {
+			//to get int value from rune c subtract the value of '0'
+			word_form += number_names[c-'0']
+		} else {
+			// FIX ME: raise an error
+			return "ERROR"
+		}
+	}
+	return word_form
 }
